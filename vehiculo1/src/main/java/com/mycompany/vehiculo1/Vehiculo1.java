7816d640c14055075c583c257cd05a8dbcd49aed
STACER-LA <110427862+Stacer-LA@users.noreply.github.com>
2022-08-22 13:46:58 -0600

    Add files via upload

---
+++ vehiculo1/src/main/java/com/mycompany/vehiculo1/Vehiculo1.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.vehiculo1;
+
+/**
+ *
+ * @author pc
+ */
+public class Vehiculo1 {
+public class vehiculo {
+    protected String color;
+    protected int ruedas;
+    
+    public String color1 (){
+        return color;
+    }
+    public void color2 (String color ){
+        this.color = color;
+    }
+    public int ruedas2(){
+        return ruedas;
+    }
+    public void ruedas2 (int ruedas){
+        this.ruedas=ruedas;
+    }
+    
+    
+    
+}
+
+    }
+
+
+
+
